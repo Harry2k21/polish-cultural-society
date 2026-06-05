670bf9e70c7dc432069b56f569be2a796759460a
Harry2k21 <88850029+Harry2k21@users.noreply.github.com>
2026-06-06 00:40:36 +0100

    fixed import issue2

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 const filterCategories = [
